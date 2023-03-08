87b1761946f249aff0a18278be7748ab4dd2b26b
David Engelen <robelene112@gmail.com>
2023-03-08 18:56:13 +0100

    Make subreddit images uniform

--- src/components/subreddits/SubReddits.js
+++ src/components/subreddits/SubReddits.js
@@ -3,21 +3,21 @@ import "./SubReddits.css";
 import "./SubReddits-mobile.css";
 import askreddit from "./assets/askreddit.png";
 import funny from "./assets/funny.png";
+import mildlyinfuriating from "./assets/mildlyinfuriating.png";
+import interestingasfuck from "./assets/interestingasf.png";
+import gaming from "./assets/gaming.png";
+import minecraft from "./assets/minecraft.png";
+import movies from "./assets/movies.png";
 
 const SubReddits = () => {
   const subReddits = {
     AskReddit: askreddit,
-    mildyinfuriating:
-      "https://styles.redditmedia.com/t5_2ubgg/styles/communityIcon_lkxajjefezh51.png?width=256&v=enabled&s=e9f08e5f20f9c160ae08bda2cfd54ad5af270323",
-    interestingasfuck:
-      "https://styles.redditmedia.com/t5_2qhsa/styles/communityIcon_5v3bepmshpg81.png?width=256&v=enabled&s=2914ade77dd2525043d2a30b44aad9ea0a38c0b9",
-    gaming:
-      "https://styles.redditmedia.com/t5_2qh03/styles/communityIcon_1isvxgkk7hw51.png?width=256&v=enabled&s=00f6d887500614cf4e45adc847b7ab075a8170a6",
-    Minecraft:
-      "https://b.thumbs.redditmedia.com/rwN0al9P6nYhGSQO-yIJb-FyF5xg-c2v61zjMom4c4E.png",
-    movies:
-      "https://styles.redditmedia.com/t5_2qh3s/styles/communityIcon_yq9ah8eniar81.jpg?width=256&format=pjpg&v=enabled&s=8f37365c563a8960ff102217387d22923856df76",
-    funny: funny,
+    Mildlyinfuriating: mildlyinfuriating,
+    Interestingasfuck: interestingasfuck,
+    Gaming: gaming,
+    Minecraft: minecraft,
+    Movies: movies,
+    Funny: funny,
   };
 
   const handleClick = ({ target }) => {
